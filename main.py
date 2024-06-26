9178bdd1142ac039499b2839ce8f8362012dc77a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-06-26 23:22:28 +0400

    Query all Movies

--- main.py
+++ main.py
@@ -44,23 +44,26 @@ with app.app_context():
     db.create_all()
 
 
-new_movie = Movie(
-    title="Phone Booth",
-    year=2002,
-    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-    rating=7.3,
-    ranking=10,
-    review="My favourite character was the caller.",
-    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-)
-
-with app.app_context():
-    db.session.add(new_movie)
-    db.session.commit()
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+#
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
 
 
 @app.route("/")
 def home():
+    result = db.session.execute(db.select(Movie))
+    all_movies = result.scalars().all()
+    print(all_movies)
     return render_template("index.html")
 
 
